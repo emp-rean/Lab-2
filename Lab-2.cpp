ea45f57bf5558c25e1b7d0a68de1d5b76adb0458
emp-rean <spforimport@gmail.com>
2024-09-30 17:34:55 +0200

    Add files via upload

---
+++ Lab-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main()
+{
+	short a;
+	cout << "enter a:";
+	cin >> a;
+	int b = pow(a,3);
+	cout << b;
+	return 0;
+}
